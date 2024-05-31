601c3145b36659b519426027a43ab4a6d9ee1c01
Domukas1337 <domukas1337@gmail.com>
2024-05-31 20:46:12 +0300

    added feed component

---
+++ src/app/components/Feed/Feed.tsx
@@ -0,0 +1,7 @@
+export function Feed(){
+    return (
+        <div className="w-screen border-2 my-2 mx-2">
+            Feed
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Feed } from "./components/Feed/Feed";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-row">
       <Sidebar />
+      <Feed />
     </main>
   );
 }
