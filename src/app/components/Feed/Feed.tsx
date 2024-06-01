aadd70f75a7c8c6fd3e36b7fd2a19783f9ca6dc3
domukas1337 <domukas1337@gmail.com>
2024-06-01 14:55:37 +0300

    testing commit from linux

--- src/app/components/Feed/Feed.tsx
+++ src/app/components/Feed/Feed.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image"
+
 export function Feed(){
     return (
         <div className="w-screen border-2 my-2 mx-2">
-            Feed
+           
         </div>
     )
 }
