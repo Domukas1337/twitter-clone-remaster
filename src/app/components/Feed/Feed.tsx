84b4b7108569cd422bb970af65cfb5ca3273743f
Domukas1337 <domukas1337@gmail.com>
2024-06-02 14:56:20 +0300

    cool feed

--- src/app/components/Feed/Feed.tsx
+++ src/app/components/Feed/Feed.tsx
@@ -3,17 +3,93 @@ import { Post } from "./PostComponent"
 
 export function Feed(){
     return (
-        <div className="flex flex-col justify-center items-center w-screen border-2 my-2 mx-2">
-           <Post 
-           name="John Doe"
-           text="Hello World"
-           profilePic="/lol.png"
-           />
-           <Post 
-           name="John Doe"
-           text="lets Welcome children of the Spring, In your garbs of green and gold"
-           profilePic="/lol.png"
-           />
+        <div className="flex flex-col justify-center items-center w-screen h-screen border-2 m-2">
+            <div className="overflow-y-scroll">
+                <Post 
+                name="John Doe"
+                text="Hello World"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+                <Post 
+                name="John Doe"
+                text="lets Welcome children of the Spring, In your garbs of green and gold asjfa jf ajsdgb adgja djgj hraig hrei g93 945u934jergberg oerg bwerhg 4u3w3"
+                profilePic="/lol.png"
+                likedCount={10}
+                retweetCount={10}
+                />
+            </div>
         </div>
     )
 }
--- src/app/components/Feed/PostComponent.tsx
+++ src/app/components/Feed/PostComponent.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image"
 
-export function Post({name, text, profilePic}: {name: string, text: string, profilePic: string}) {
+export function Post({name, text, profilePic, likedCount, retweetCount}: {name: string, text: string, profilePic: string, likedCount: number, retweetCount: number}) {
     return (
         <div className="border-2 rounded-md p-2 w-1/2 h-fit mx-auto my-2">
             <div>
                 <Image src={profilePic} alt="Profile picture" width={50} height={50} />
             </div>
-            <div className="text-xl text-white">
+            <div className="text-2xl text-white">
                 {name}
             </div>
-            <div className="text-xl text-white">
+            <hr />
+            <div className="text-l text-white">
                 {text}
+            </div>
+            <hr />
+            <div className="flex flex-row text-gray-200 gap-2 my-2">
+                <button className="border-2 rounded border-purple-400 p-2 hover:bg-purple-400 transition">{likedCount} likes</button>
+                <button className="border-2 rounded border-white p-2 hover:bg-white hover:text-black transition">{retweetCount} retweets</button>
             </div>
         </div>
     )
